2acbc744137f82b2d94b3f80191eb8560b474758
nethajipersonal <161015791+nethajipersonal@users.noreply.github.com>
2024-06-26 10:54:02 +0530

    Updates

---
+++ assets/fonts/fontSize.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  Title: 40,
+  DiscriptionLite: 32,
+  Heading: 18,
+  Discription:20,
+  HeadingLite: 15,
+  ButtonTitle:20,
+  TextButton:20,
+  MobileTitle:25,
+  MobileDiscriptionLite: 15,
+  MobileHeading: 24,
+  MobileHeadingLite: 15,
+  MobileButtonTitle:14,
+  MobileDiscription:12,
+  MobileTextButton:14,
+};
---
+++ assets/fonts/fonts.js
@@ -0,0 +1,6 @@
+export default {
+  primaryLight: 'AmpleSoftPro-Light',
+  primaryRegular: 'AmpleSoftPro-Regular',
+  primaryBold: 'AmpleSoftPro-Bold',
+  primarySemiBold: 'AmpleSoftPro-Medium',
+};
